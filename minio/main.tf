089b05b7608541349623799733218f084e90f54d
daun-gatal <rsudo54@gmail.com>
2025-10-12 16:41:51 +0700

    add gitlab ci

--- minio/main.tf
+++ minio/main.tf
@@ -22,7 +22,7 @@ provider "helm" {
 }
 
 module "minio" {
-  source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/minio?ref=update-airflow"
+  source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/minio?ref=main"
 
   minio_root_password = var.minio_root_password
   tailscale_expose    = true
