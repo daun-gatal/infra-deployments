7ce103e5be84278ab69ce9d47b4107e441976b1a
daun-gatal <rsudo54@gmail.com>
2025-10-13 20:30:52 +0700

    add kafka

--- kafka/main.tf
+++ kafka/main.tf
@@ -1,3 +1,27 @@
+terraform {
+  backend "http" {}
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.30.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
 module "kafka" {
   source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/kafka?ref=main"
 
@@ -14,4 +38,9 @@ module "kafka" {
   kafka_ui_tailscale_expose = true
   kafka_ui_auth_enabled = true
   kafka_ui_auth_password = var.kafka_ui_auth_password
+}
+
+output "kafka_int_bootstrap_servers" {
+  description = "Kafka bootstrap servers connection string for client applications"
+  value       = module.kafka.kafka_int_bootstrap_servers
 }
