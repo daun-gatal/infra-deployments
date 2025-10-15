2d02539777f1d053b23ae9ce478230d49bf6be48
daun-gatal <rsudo54@gmail.com>
2025-10-15 08:02:54 +0700

    add spark

---
+++ spark/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "http" {}
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.30.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+module "spark" {
+  source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/spark?ref=main"
+
+  tailscale_expose = true
+  spark_connect_executor_memory = "8g"
+  cluster_worker_count = 2
+  spark_connect_max_cores = 2
+}
