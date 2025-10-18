43627aa88943573a5b39b3e170349adc4827e8c4
daun-gatal <rsudo54@gmail.com>
2025-10-18 21:27:32 +0000

    add metabase

---
+++ metabase/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "http" {}
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.30.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+locals {
+  db = jsondecode(file(var.db_credentials_path))
+}
+
+module "metabase" {
+  source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/metabase?ref=main"
+
+  metabase_db_password = local.db.postgres_password.value
+  metabase_db_user     = local.db.postgres_username.value
+  metabase_db_host     = local.db.postgres_rw_dns.value
+  metabase_db_port     = 5432
+  metabase_db_name     = "metabase"
+  tailscale_expose     = true
+}
---
+++ metabase/variables.tf
@@ -0,0 +1,5 @@
+variable "db_credentials_path" {
+  description = "Path to the file containing database credentials"
+  type        = string
+  sensitive = true
+}
