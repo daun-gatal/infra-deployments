99ed2f74dd35636fbbfee62571c0872b42296fa6
daun-gatal <rsudo54@gmail.com>
2025-12-02 07:43:10 +0700

    update template

--- airflow/variables.tf
+++ airflow/variables.tf
@@ -1,12 +1,36 @@
-variable "db_credentials_path" {
-  description = "Path to the file containing database credentials"
+variable "db_password" {
+  description = "The password for the Postgres database user."
+  type        = string
+  sensitive   = true
+}
+
+variable "db_user" {
+  description = "The user of Postgres database."
   type        = string
   sensitive = true
 }
 
-variable "minio_credentials_path" {
-  description = "Path to the file containing MinIO credentials"
+variable "db_internal_dns" {
+  description = "The internal DNS of Postgres database."
   type        = string
+  sensitive = true
+}
+
+variable "minio_root_password" {
+  description = "The root password of MINIO"
+  type = string
+  sensitive = true
+}
+
+variable "minio_root_user" {
+  description = "The root user of MINIO"
+  type = string
+  sensitive = true
+}
+
+variable "minio_internal_dns" {
+  description = "The internal DNS of MINIO"
+  type = string
   sensitive = true
 }
 
