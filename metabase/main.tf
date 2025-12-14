a433636a73a9391fa82856feea33c73a06767df1
daun-gatal <rsudo54@gmail.com>
2025-12-14 09:49:24 +0000

    Update file main.tf

--- metabase/main.tf
+++ metabase/main.tf
@@ -22,16 +22,16 @@ provider "helm" {
   }
 }
 
-module "metabase" {
-  source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/metabase?ref=main"
+# module "metabase" {
+#   source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/metabase?ref=main"
 
-  metabase_db_password = var.db_password
-  metabase_db_user     = var.db_user
-  metabase_db_host     = var.db_internal_dns
-  metabase_db_port     = 5432
-  metabase_db_name     = "metabase"
-  tailscale_expose     = true
-  tailscale_funnel    = true
-}
+#   metabase_db_password = var.db_password
+#   metabase_db_user     = var.db_user
+#   metabase_db_host     = var.db_internal_dns
+#   metabase_db_port     = 5432
+#   metabase_db_name     = "metabase"
+#   tailscale_expose     = true
+#   tailscale_funnel    = true
+# }
 
 # Add comments here v6
