fbe415482952e9b86b7bb98ef4b01f4e8608d602
daun-gatal <rsudo54@gmail.com>
2025-12-15 14:56:39 +0000

    Update file main.tf

--- airbyte/main.tf
+++ airbyte/main.tf
@@ -23,165 +23,93 @@ provider "helm" {
 }
 
 
+locals {
+  components_with_minio_env = [
+    "webapp",
+    "worker",
+    "workloadLauncher",
+    "connectorRolloutWorker",
+    "metrics",
+    "airbyteBootloader",
+    "temporal",
+    "temporalUi",
+    "cron",
+  ]
+
+  minio_extra_env = [
+    {
+      name = "MINIO_ENDPOINT"
+      valueFrom = {
+        secretKeyRef = {
+          name = "airbyte-config-secrets"
+          key  = "MINIO_ENDPOINT"
+        }
+      }
+    }
+  ]
+
+  component_envs = {
+    for c in local.components_with_minio_env :
+    c => { extraEnv = local.minio_extra_env }
+  }
+}
+
 module "airbyte" {
   source = "git::https://gitlab.com/daun-gatal/terraform-modules.git//modules/airbyte?ref=main"
 
-  values = {
-    global = {
-      database = {
-        type = "external"
+  values = merge(
+    {
+      global = {
+        database = {
+          type = "external"
+          secretName = "airbyte-config-secrets"
+          host = "postgres-cluster-rw.database.svc.cluster.local"
+          port = 5432
+          name = "airbyte"
+          userSecretKey = "DB_USER"
+          passwordSecretKey = "DB_PASS"
+        }
+        storage = {
+          secretName = "airbyte-config-secrets"
+          type = "minio"
+        }
+      }
+
+      fullnameOverride = "airbyte"
+
+      postgresql = {
+        enabled = false
+      }
+
+      minio = {
+        enabled = false
         secretName = "airbyte-config-secrets"
-        host = "postgres-cluster-rw.database.svc.cluster.local"
-        port = 5432
-        name = "airbyte"
-        userSecretKey = "DB_USER"
-        passwordSecretKey = "DB_PASS"
       }
-      storage = {
-        secretName = "airbyte-config-secrets"
-        type = "minio"
+
+      server = {
+        service = {
+          type = "ClusterIP",
+          port = 80
+          annotations = {
+            "tailscale.com/expose" = "true"
+            "tailscale.com/hostname" = "airbyte-web-int"
+          }
+        }
+        extraEnv = [
+          {
+            name = MINIO_ENDPOINT
+            valueFrom = {
+              secretKeyRef = {
+                name = "airbyte-config-secrets"
+                key = "MINIO_ENDPOINT"
+              }
+            }
+          }
+        ]
       }
-    }
-
-    fullnameOverride = "airbyte"
-
-    postgresql = {
-      enabled = false
-    }
-
-    minio = {
-      enabled = false
-      secretName = "airbyte-config-secrets"
-    }
-
-    webapp = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    worker = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    workloadLauncher = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    connectorRolloutWorker = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    metrics = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    airbyteBootloader = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    temporal = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    temporalUi = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    cron = {
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-
-    server = {
-      service = {
-        type = "ClusterIP",
-        port = 80
-        annotations = {
-          "tailscale.com/expose" = "true"
-          "tailscale.com/hostname" = "airbyte-web-int"
-        }
-      }
-      extraEnv = [
-        name = MINIO_ENDPOINT
-        valueFrom = {
-          secretKeyRef = {
-            name = "airbyte-config-secrets"
-            key = "MINIO_ENDPOINT"
-          }
-        }
-      ]
-    }
-  }
+    },
+    local.component_envs
+  )
 }
 
 # Add comments here if needed v2
