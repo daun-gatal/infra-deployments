cd2cadd17cc0c9b6a9626253827fc7739f29302d
daun-gatal <rsudo54@gmail.com>
2025-12-17 15:04:08 +0000

    Edit main.tf

--- openbao/main.tf
+++ openbao/main.tf
@@ -27,6 +27,7 @@ module "openbao" {
 
   tailscale_ui_expose    = true
   generate_unseal_key = false
+  image_tag = "2.5.0-beta20251125"
 
   server_storage_secret_name = "openbao-storage-config"
   server_unseal_secret_name = "openbao-unseal-key"
