1b45e66ddba78cff9d668eb735abc77cc144019a
daun-gatal <rsudo54@gmail.com>
2025-12-17 14:38:29 +0000

    Update 3 files

--- openbao/main.tf
+++ openbao/main.tf
@@ -26,12 +26,15 @@ module "openbao" {
   source = "git::ssh://git@gitlab.com/daun-gatal/terraform-modules.git//modules/openbao?ref=main"
 
   tailscale_ui_expose    = true
+  generate_unseal_key = false
 
-  server_storage_secret_name = "openbao-storage-config-v1"
-  server_unseal_secret_name = "openbao-unseal-key-v1"
-  unseal_previous_key = "RHz3ki3SPdNYt29OATyK7JqhWMvHBI0+ML571cbe4dA="
-  unseal_previous_key_id = "12345-1"
-  unseal_current_key_id = "20251217-1"
+  server_storage_secret_name = "openbao-storage-config"
+  server_unseal_secret_name = "openbao-unseal-key"
+  
+  unseal_previous_key = var.unseal_previous_key
+  unseal_previous_key_id = var.unseal_previous_key_id
+  unseal_current_key_id = var.unseal_current_key_id
+  unseal_current_key = var.unseal_current_key
   
   storage_type = "postgresql"
   storage_postgresql = {
