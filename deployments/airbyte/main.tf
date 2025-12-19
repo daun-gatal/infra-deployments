c523906b6cd76c1e02df90d1510140727b05dd74
daun-gatal <rsudo54@gmail.com>
2025-12-19 16:05:09 +0700

    refactor ci

--- deployments/airbyte/main.tf
+++ deployments/airbyte/main.tf
@@ -1,31 +1,4 @@
-terraform {
-  backend "http" {}
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.30.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "3.0.2"
-    }
-  }
-}
 
-provider "kubernetes" {
-  config_path = ""
-}
-
-provider "helm" {
-  kubernetes = {
-    config_path = ""
-  }
-}
-
-
-# module "airbyte" {
-#   source       = "git::https://gitlab.com/daun-gatal/terraform-modules.git//modules/airbyte?ref=main"
-#   release_name = local.release_name
 # module "airbyte" {
 #   source       = "git::https://gitlab.com/daun-gatal/terraform-modules.git//modules/airbyte?ref=main"
 #   release_name = local.release_name
@@ -35,10 +8,6 @@ provider "helm" {
 #     local.component_envs
 #   )
 # }
-#   values = merge(
-#     local.values,
-#     local.component_envs
-#   )
-# }
+
 
 # Add comments here if needed v8
