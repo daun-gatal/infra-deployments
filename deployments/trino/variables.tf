cd25d27cd35ecddedc5c186544304251db7fdba2
daun-gatal <rsudo54@gmail.com>
2026-01-05 17:42:19 +0700

    feat(trino): Switch Iceberg to HMS and add Delta catalog

--- deployments/trino/variables.tf
+++ deployments/trino/variables.tf
@@ -4,38 +4,20 @@ variable "trino_shared_secret" {
   sensitive   = true
 }
 
-variable "db_password" {
-  description = "The password for the Postgres database user."
+variable "minio_root_password" {
+  description = "The root password of MINIO"
   type        = string
   sensitive   = true
 }
 
-variable "db_user" {
-  description = "The user of Postgres database."
-  type        = string
-  sensitive = true
-}
-
-variable "db_internal_dns" {
-  description = "The internal DNS of Postgres database."
-  type        = string
-  sensitive = true
-}
-
-variable "minio_root_password" {
-  description = "The root password of MINIO"
-  type = string
-  sensitive = true
-}
-
 variable "minio_root_user" {
   description = "The root user of MINIO"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "minio_internal_dns" {
   description = "The internal DNS of MINIO"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
